1decf41556e46470224fa126fc5585efccb75fa3
Pavel <shipilopavel92@gmail.com>
2020-03-31 01:01:17 +0300

    added new waiters page, added some codestyle fixes, fixed test

---
+++ pages/geoPage.js
@@ -0,0 +1,16 @@
+let waiters = require('../pages/waiters')
+
+const geoPage = function() { 
+    let searchField = element(by.id('city__front-input'));
+    let searchFieldDropdown = element(by.css('.popup__items'));
+    let searchFieldDropdownFrstEl = element(by.css('ul:first-child'));
+            
+    this.changeGeolocation = async function(city) {
+        waiters.elementIsClickable(searchField);        
+        await searchField.clear();
+        await searchField.sendKeys(city);
+        waiters.elementIsVisible(searchFieldDropdownFrstEl);
+        await searchFieldDropdownFrstEl.click();         
+    };
+};
+module.exports = new geoPage();
